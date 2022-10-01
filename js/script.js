1b581db159f603bec04123c7e86333cb0cae2bc8
Artem <halimov98@list.ru>
2022-10-01 22:36:03 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+'use sctrict';
+
+// a= 15;
+// console.log(a);
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+// Интерплоляция
+const category =  "toys";
+
+console.log(`http://localhost.com/${category}/5`);
+
+const user = "Ivan";
+alert(`Hello,${user}`);
+
