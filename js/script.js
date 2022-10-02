17b91231453ffade604c60c3c0fe731944e89185
Artem <halimov98@list.ru>
2022-10-02 16:16:24 +0500

    a commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-'use sctrict';
+"use strict";
 
 // a= 15;
 // console.log(a);
@@ -18,11 +18,58 @@
 
 // console.log(obj);
 
-// Интерплоляция
-const category =  "toys";
+// *Интерплоляция//
+// const category =  "toys";
 
-console.log(`http://localhost.com/${category}/5`);
+// console.log(`http://localhost.com/${category}/5`);
 
-const user = "Ivan";
-alert(`Hello,${user}`);
+// const user = "Ivan";
+// alert(`Hello,${user}`);
 
+//унарный плюс//
+// console.log(4 + +"5")
+
+
+// let incr = 10,
+//     decr = 10;
+
+// ++incr;//инкремент префексный
+// --decr;//дикремент префексный
+
+// incr++;//инкремент постфексный
+// decr--;//дикремент постфексный
+
+// console.log(incr);
+
+// console.log(decr);
+
+// console.log(5%2);
+
+// console.log(2*2 == 4);
+
+// console.log(2*2 == "4");
+
+// console.log(2*2 === "4");
+
+// console.log(2*2 != 5);
+// console.log(2*2 !== "4");
+
+// // && 
+// // ||
+
+// const inChecked = true,
+//     inClose = false;;
+
+// console.log(inChecked && inClose);
+
+// const inChecked = true,
+//     inClose = false;;
+
+// console.log(inChecked || inClose);
+
+// // !
+
+// const inChecked = false,
+//     inClose = false;;
+
+// console.log(inChecked || !inClose);
