4c1ecb665553fffe40a320e662428ddd0bde3c95
Artem <halimov98@list.ru>
2022-10-02 17:34:42 +0500

    1projct

--- js/script.js
+++ js/script.js
@@ -73,3 +73,23 @@
 //     inClose = false;;
 
 // console.log(inChecked || !inClose);
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+
+const personalMovidDB = {
+    count:numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', ''); 
+
+personalMovidDB.movies[a] = b;
+personalMovidDB.movies[c] = d;
+
+console.log(personalMovidDB);
