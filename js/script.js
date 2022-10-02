59e53d46c97524820dce382c61479efe1ecc9324
Artem <halimov98@list.ru>
2022-10-02 17:56:46 +0500

    terms

--- js/script.js
+++ js/script.js
@@ -74,22 +74,61 @@
 
 // console.log(inChecked || !inClose);
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+//* первый проект *//
 
-const personalMovidDB = {
-    count:numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', ''); 
+// const personalMovidDB = {
+//     count:numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-personalMovidDB.movies[a] = b;
-personalMovidDB.movies[c] = d;
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//     b = prompt('На сколько оцените его?', ''),
+//     c = prompt('Один из последних просмотренных фильмов?', ''),
+//     d = prompt('На сколько оцените его?', ''); 
 
-console.log(personalMovidDB);
+// personalMovidDB.movies[a] = b;
+// personalMovidDB.movies[c] = d;
+
+// console.log(personalMovidDB);
+
+// ***условия ///
+
+if (4 == 9) {
+    console.log('Ok');
+} else {
+    console.log('Err');
+}
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Err');
+// } else if (num > 100) {
+//     console.log('много');
+// }else {
+//     console.log('ok');
+// }
+
+// (num === 50) ? console.log('ok') : console.log('Err');  // тернальный оператор
+
+const num = 50;
+
+switch (num) {  // стогое соответсвие
+    case 49:
+        console.log('err');
+        break;
+    case 100:
+        console.log('err');
+        break;
+    case 50:
+        console.log('в точку');
+        break;
+    default:
+        console.log('не в этот раз');
+        break;
+}
