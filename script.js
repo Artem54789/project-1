859011c449a5d045180c491670d2b689b98b8950
Artem <halimov98@list.ru>
2022-10-11 20:38:47 +0500

    сосновы ООП, прототипно принетированное наследование

--- script.js
+++ script.js
@@ -500,9 +500,16 @@ P.S. Функции вызывать не обязательно*/
 // console.log(counter);
 
 
-//*** массивы и псевдомассивы ***//
+//*** массивы и псевдомассивы  ***//
 
-// const arr = [1,2,4,5,8];
+// const arr = [11,22,4,5,12];
+// arr.sort(compateNum);
+// console.log(arr);
+
+// function compateNum (a, b) {
+//     return a - b;
+// }
+
 // arr[99] = 0;
 // console.log(arr.length); // последний индекс +1
 // console.log(arr);
@@ -524,7 +531,120 @@ P.S. Функции вызывать не обязательно*/
 //     console.log(value);
 // }
 
-const str = prompt("","");
-const products = str.split(", ");
-// console.log(products);
-console.log(products.join(";"));
+// const str = prompt("","");
+// const products = str.split(", "); 
+// products.sort();
+// // console.log(products);
+// console.log(products.join(";"));
+
+// *** передача по ссылке или по значению ,Spread оператор *** //
+
+// let a = 5,
+//     b = a;
+// b = b + 5;
+
+// console.log(a);
+
+// console.log(b);
+
+
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
+
+// const copy = obj; // Cсылку передали
+
+// copy.a = 10;
+
+// console.log(copy);
+
+// console.log(obj);
+
+// function copy(mainObj){
+//     let objCopy = {};
+
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+
+//     return objCopy;
+// }
+
+// const numbers ={
+//     a:2,
+//     b:5,
+//     c: {
+//         x:7,
+//         y:4
+//     }
+// };
+
+// const newNumbers = copy(numbers);// поверхностная копия 
+
+// newNumbers.a= 10;
+// newNumbers.c.x= 10;
+
+// console.log(newNumbers);
+// console.log(numbers);
+
+
+// const add = {
+//     d: 17,
+//     e: 20
+// };
+// const clone = Object.assign({},add);
+
+// clone.d =20;
+
+// console.log(add);
+
+// console.log(clone);
+
+// const oldArray = ["a", "c", "d"];
+// const newArray = oldArray.slice();
+
+// newArray[1] = 'фывфыв';
+
+// console.log(newArray);
+// console.log(oldArray);
+
+
+// const video = ['youtube','vimeo','rutube'],
+//     blogs = ['wordpress', 'livejournal', 'blogger'],
+//     internet = [...video, ...blogs, 'vk'];
+
+// console.log(internet);
+
+
+// function log (a,b,c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// const num = [2,3,6];
+
+// log(...num);
+
+// const array = ['a','b'];
+
+// const newAarray = [...array];
+
+// const q = {
+//     one:1, 
+//     two:2
+// };
+
+// const newObj = {...q};
+
+//***  сосновы ООП, прототипно принетированное наследование ***//
+
+let str ="test";
+let strObj = new String();
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+console.dir([1,2,3]);
