50b22bc80f5220a646ad1e2eee8fa658534553e3
Artem <halimov98@list.ru>
2022-10-12 22:03:59 +0500

    Практика 4ч. Используем объекты

--- script.js
+++ script.js
@@ -641,10 +641,116 @@ P.S. Функции вызывать не обязательно*/
 
 //***  сосновы ООП, прототипно принетированное наследование ***//
 
-let str ="test";
-let strObj = new String();
+// let str ="test";
+// let strObj = new String();
 
 // console.log(typeof(str));
 // console.log(typeof(strObj));
 
-console.dir([1,2,3]);
+// console.dir([1,2,3]);
+
+// const solfier = {
+//     health: 400,
+//     armor: 100,
+//     sayHello : function() {
+//         console.log("hello");
+//     }
+// };
+
+// const jonh = Object.create(solfier);// создание прототипных связей
+
+// const jonh = {
+//     health: 100
+// };
+
+// jonh.__proto__ = solfier; //старая версия
+
+// Object.setPrototypeOf(jonh,solfier);//новая версия
+
+// console.log(jonh.armor);
+// jonh.sayHello();
+
+
+//***Практика 4ч. Используем объекты***//
+
+/* Задание на урок:
+
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+
+const personalMovidDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+    start: function () {
+        personalMovidDB.count = +prompt('Сколько фильмов вы уже посмотрели?','');
+    
+        while (personalMovidDB.count == ""|| personalMovidDB.count == null || isNaN(personalMovidDB.count)) {
+            personalMovidDB.count = +prompt('Сколько фильмов вы уже посмотрели?','');
+        }
+    },
+    rememberMyFilms: function()  {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', ''),
+                b = prompt('На сколько оцените его?', '');
+        
+                if (a != null && a!= "" && a.length < 50 && b != null && b != "" ) {
+                    personalMovidDB.movies[a] = b;
+                    console.log('done');
+                } else {
+                    console.log('error');
+                    i--;
+                }
+            } 
+    },
+    detectPersonalLevel: function() {
+        if (personalMovidDB.count < 10){
+            console.log ("Просмотрено довольно мало фильмов");
+        } else if (personalMovidDB.count >= 10 && personalMovidDB.count < 30){
+            console.log ("Вы классический зритель");
+        }else if (personalMovidDB.count >= 30 && personalMovidDB.count < 50) {
+            console.log ("Вы киноман");
+        }else {
+            console.log ("Err");
+        }
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log (personalMovidDB);
+        }
+    },
+    toggleVisibleMyDB: function() {
+        if (personalMovidDB.privat) {
+            personalMovidDB.privat = false;
+        }else {
+            personalMovidDB.privat = true;
+        }
+    },
+    writeYourGenres: function () {
+    for (let i = 1; i <= 3; i++) {
+        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+
+        if(genre === ''|| genre === null){
+            console.log('вы ввели некорректные данные')
+            i--
+        }else{
+            personalMovidDB.genres[i-1] = genre;
+        }
+    }
+    personalMovidDB.genres.forEach((item, i) => {
+        console.log(`"Любимый жанр ${i + 1}- это ${item}"`)
+    });
+}
+};
+
