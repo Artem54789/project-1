90fe30e7c60dcfce62e7d4d8b964cd0075d05227
Artem <halimov98@list.ru>
2022-10-14 17:36:13 +0500

    тест на понимание

--- script.js
+++ script.js
@@ -812,3 +812,36 @@ P.S. Функции вызывать не обязательно*/
 // console.log(typeof(!!'444'));
 
 // Задачи с собеседований на понимание основ
+
+// let x = 5; alert(x++);
+
+// [] + false - null + true
+
+// console.log(typeof([] + false )); //"false"
+// console.log(typeof([] + false - null )); //NaN
+// console.log(typeof([] + false - null + true));//NaN
+
+// let y = 1;
+// let x = y = 2;
+// alert(x);  //2
+
+// console.log([] + 1 + 2);//12
+
+// alert("1"[0]); // 0 это индекс
+
+// console.log(2 && 1 && null && 0 && undefined); 0
+// И запинается на false 
+// ИЛИ запинается на true 
+
+// console.log(!!(1 && 2) === (1 && 2));// !! булиновая форма
+
+//              2    1     3
+// alert (null || 2  && 3 || 4); 
+
+// let a = [1,2,3],
+//     b = [1,2,3];
+// console.log(a === b); 2 разных ящика
+
+alert(typeof(+"sada") );
+
+
